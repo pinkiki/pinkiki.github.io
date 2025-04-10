67ddd58e67f2593975dbffcd2a5837837cabc0ed
pinkiki <cmq558866@tom.com>
2025-04-10 22:04:04 +0800

    Site updated: 2025-04-10 22:04:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello/","interview-me/","nuxt-study/","vue3-study/","project-summary/","wechat-qrcode/"];function toRandomPost(){
+var posts=["nuxt-study/","hello/","interview-me/","project-summary/","vue3-study/","wechat-qrcode/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
