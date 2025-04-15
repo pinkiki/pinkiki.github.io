719783999460ce389806702da995bc9f38ee0756
pinkiki <cmq558866@tom.com>
2025-04-15 17:38:05 +0800

    Site updated: 2025-04-15 17:38:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello/","Elpis/","nuxt-study/","interview-me/","vue3-study/","project-summary/","wechat-qrcode/"];function toRandomPost(){
+var posts=["Elpis/","interview-me/","hello/","nuxt-study/","project-summary/","wechat-qrcode/","vue3-study/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
