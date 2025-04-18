43992db3ea8ac7e8aca41e375be6a59cbd2d3899
cmeng <2542966194@qq.com>
2025-04-18 18:03:22 +0800

    Site updated: 2025-04-18 18:03:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Elpis/","interview-me/","nuxt-study/","project-summary/","hello/","wechat-qrcode/","vue3-study/"];function toRandomPost(){
+var posts=["configuration/","Elpis/","nuxt-study/","project-summary/","hello/","wechat-qrcode/","interview-me/","vue3-study/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
